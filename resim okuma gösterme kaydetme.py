077f231a092662c5602050af6d141c2c88d7740a
aliihsanbadem <59990046+aliihsanbadem@users.noreply.github.com>
2020-12-12 22:03:25 +0300

    Add files via upload
    Resim okuma, gösterme ve kaydetme

---
+++ resim okuma gösterme kaydetme.py
@@ -0,0 +1,11 @@
+import cv2
+
+img = cv2.imread("klon.jpg",0)
+#print(img)
+cv2.namedWindow("Image",cv2.WINDOW_NORMAL)
+
+cv2.imshow("Image",img)
+cv2.imwrite("klon1.jpg",img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
